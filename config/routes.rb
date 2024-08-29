d7ca20b960485a7cf4860a02f5adee6b27dbb465
Jerome <fabricantejerome@gmail.com>
2024-08-29 21:53:00 +0800

    Handle blog form submit and validation

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   root "blog_posts#index"
 end
